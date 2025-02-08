4ba7ac1b96ca24ce6137c0e1256cfc6500b00ef2
ThanuraSilva <thanurasilva1989@gmail.com>
2025-02-08 11:08:40 +0530

    Test events with functions decalrings

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,7 +1,22 @@
 export const Demo = () =>{
+    let num: number;
+    //  add value
+    const handleOnClick = () =>{
+        alert("Added OK")
+        console.log("hello")
+        // num += 1
+        // console.log(num)
+    }
+
     return(
         <>
-          <h2>Hello Demo Component</h2>
+          <input type="text" readOnly style={{marginTop:"60px",marginLeft:"100px"}}/>
+          {/* reference a function */}
+          <button onClick={handleOnClick}>Click me to +</button>
+          {/* inline function */}
+          {/* <button onClick={()=>console.log("Hello Button ")}>Click me to +</button> */}
+          <button>Click me to -</button>
         </>
+       
     );
 }
